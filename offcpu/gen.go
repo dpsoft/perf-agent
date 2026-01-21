37864147c2178f03aa0570d9f0630588f317a51c
diego <diegolparra@gmail.com>
2026-01-21 16:50:15 -0300

    Rename BPF files for ARM64 architecture and update CI configurations for multi-architecture builds

--- offcpu/gen.go
+++ offcpu/gen.go
@@ -1,3 +1,4 @@
 package offcpu
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -go-package=offcpu Offcpu ../bpf/offcpu.bpf.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 -go-package=offcpu Offcpu ../bpf/offcpu.bpf.c -- -I../bpf
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target arm64 -go-package=offcpu Offcpu ../bpf/offcpu.bpf.c -- -I../bpf

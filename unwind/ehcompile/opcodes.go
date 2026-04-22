14b6565f878ca5e64a6ef7d22946bbb1cf9793e4
diego <diegolparra@gmail.com>
2026-04-22 11:03:28 -0300

    ehcompile: gofmt opcodes.go

--- unwind/ehcompile/opcodes.go
+++ unwind/ehcompile/opcodes.go
@@ -7,29 +7,29 @@ const (
 	cfaOffset     = 0x80 // top 2 bits == 10; low 6 bits = register
 	cfaRestore    = 0xc0 // top 2 bits == 11; low 6 bits = register
 
-	cfaNop                = 0x00
-	cfaSetLoc             = 0x01
-	cfaAdvanceLoc1        = 0x02
-	cfaAdvanceLoc2        = 0x03
-	cfaAdvanceLoc4        = 0x04
-	cfaOffsetExtended     = 0x05
-	cfaRestoreExtended    = 0x06
-	cfaUndefined          = 0x07
-	cfaSameValue          = 0x08
-	cfaRegister           = 0x09
-	cfaRememberState      = 0x0a
-	cfaRestoreState       = 0x0b
-	cfaDefCFA             = 0x0c
-	cfaDefCFARegister     = 0x0d
-	cfaDefCFAOffset       = 0x0e
-	cfaDefCFAExpression   = 0x0f
-	cfaExpression         = 0x10
-	cfaOffsetExtendedSF   = 0x11
-	cfaDefCFASF           = 0x12
-	cfaDefCFAOffsetSF     = 0x13
-	cfaValOffset          = 0x14
-	cfaValOffsetSF        = 0x15
-	cfaValExpression      = 0x16
+	cfaNop              = 0x00
+	cfaSetLoc           = 0x01
+	cfaAdvanceLoc1      = 0x02
+	cfaAdvanceLoc2      = 0x03
+	cfaAdvanceLoc4      = 0x04
+	cfaOffsetExtended   = 0x05
+	cfaRestoreExtended  = 0x06
+	cfaUndefined        = 0x07
+	cfaSameValue        = 0x08
+	cfaRegister         = 0x09
+	cfaRememberState    = 0x0a
+	cfaRestoreState     = 0x0b
+	cfaDefCFA           = 0x0c
+	cfaDefCFARegister   = 0x0d
+	cfaDefCFAOffset     = 0x0e
+	cfaDefCFAExpression = 0x0f
+	cfaExpression       = 0x10
+	cfaOffsetExtendedSF = 0x11
+	cfaDefCFASF         = 0x12
+	cfaDefCFAOffsetSF   = 0x13
+	cfaValOffset        = 0x14
+	cfaValOffsetSF      = 0x15
+	cfaValExpression    = 0x16
 
 	cfaGnuArgsSize               = 0x2e
 	cfaGnuNegativeOffsetExtended = 0x2f
@@ -47,8 +47,8 @@ const (
 	x86RBX = 3
 	x86RSI = 4
 	x86RDI = 5
-	x86RBP = 6  // FP on x86_64
-	x86RSP = 7  // SP on x86_64
+	x86RBP = 6 // FP on x86_64
+	x86RSP = 7 // SP on x86_64
 	x86R8  = 8
 	x86R15 = 15
 	x86RIP = 16 // conventional "return address column" on x86_64
